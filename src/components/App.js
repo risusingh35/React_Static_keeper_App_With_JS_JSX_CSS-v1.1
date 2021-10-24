2da9aca1ae3aa83064fa46dafb4464632cbe03f2
Risu Singh <risusingh35@gmail.com>
2021-10-24 17:01:50 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* Arrow function, map() and props object */}
+      {Notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
